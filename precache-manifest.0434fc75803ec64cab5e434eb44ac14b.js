8cb9c3428266e05e068527559fb6a38662cffccf
rohitmirchandani <rohitmirchandani365@gmail.com>
2023-09-20 21:35:26 +0530

    Updates

---
+++ precache-manifest.0434fc75803ec64cab5e434eb44ac14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87222e188396bd2c2d7",
+    "url": "/multilingual-resume/static/css/main.3203611c.chunk.css"
+  },
+  {
+    "revision": "f87222e188396bd2c2d7",
+    "url": "/multilingual-resume/static/js/main.f87222e1.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/multilingual-resume/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "481181a0f5375fcf3319",
+    "url": "/multilingual-resume/static/js/2.481181a0.chunk.js"
+  },
+  {
+    "revision": "793ad129a16e5e9de10c",
+    "url": "/multilingual-resume/static/js/runtime~main.793ad129.js"
+  },
+  {
+    "revision": "55b5428a166d59eb6e8e8af8b0a907cf",
+    "url": "/multilingual-resume/index.html"
+  }
+];
