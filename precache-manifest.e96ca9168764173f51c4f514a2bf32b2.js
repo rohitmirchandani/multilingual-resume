ae1981d8979dba277e78ae956099c26011d4aad6
rohitmirchandani <rohitmirchandani365@gmail.com>
2023-09-20 20:34:20 +0530

    gh-pages

---
+++ precache-manifest.e96ca9168764173f51c4f514a2bf32b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f87222e188396bd2c2d7",
+    "url": "/static/css/main.3203611c.chunk.css"
+  },
+  {
+    "revision": "f87222e188396bd2c2d7",
+    "url": "/static/js/main.f87222e1.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "481181a0f5375fcf3319",
+    "url": "/static/js/2.481181a0.chunk.js"
+  },
+  {
+    "revision": "bc413e6e8d2e0ea877bc",
+    "url": "/static/js/runtime~main.bc413e6e.js"
+  },
+  {
+    "revision": "8043498fe4d7606aca9e76c877cc862e",
+    "url": "/index.html"
+  }
+];
